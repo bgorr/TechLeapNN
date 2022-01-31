0985f68eec7204dcb0521fc6913cb9927dca2af5
gapaza <gapaza@tamu.edu>
2022-01-30 23:05:27 -0600

    updates

--- neuralnet/api.py
+++ neuralnet/api.py
@@ -34,7 +34,7 @@ class NeuralNetClient:
 
         # --> Model Parameters
         self.device = 'cuda'
-        self.batch_size = 512
+        self.batch_size = 1024
         self.num_classes = 2
         self.threshold = torch.Tensor([.666]).to(self.device)
 
